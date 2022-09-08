6d38e52238d58c63017540a6ba64e8d018491811
Anastasiia <nastikue@gmail.com>
2022-09-08 16:38:09 +0300

    Setup postgresql db, add @nestjs/config to save secrets seperately from repo, update .gitignore (#1)
    Co-authored-by: Anastasiia Shapovalova <a.shapovalova@eleks.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { MemoryCardsModule } from './memory-cards/memory-cards.module';
 import { AuthModule } from './auth/auth.module';
+import configuration from '../config/configuration';
 
 @Module({
-  imports: [MemoryCardsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env'}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [User],
+      ...configuration().database
+    }),
+    MemoryCardsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
